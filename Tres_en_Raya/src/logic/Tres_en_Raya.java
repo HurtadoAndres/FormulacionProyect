f18a4b9b266eea03874939c5dc2e301761a4a047
HurtadoAndres <33639914+HurtadoAndres@users.noreply.github.com>
2019-10-01 21:00:21 -0500

    Update Tres_en_Raya.java

--- Tres_en_Raya/src/logic/Tres_en_Raya.java
+++ Tres_en_Raya/src/logic/Tres_en_Raya.java
@@ -5,138 +5,208 @@
  */
 package logic;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
-
+import java.util.Vector;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import view.Vista;
 
 /**
  *
- * @author Miller Olaya
+ * @author Estudiante
  */
 public class Tres_en_Raya {
-     public static  boolean J1= false,J2= false;
-     public  int NJ1=0;
-     public  int NJ2=0;
-     //la x dice el turno del jugador
+
+    /**
+     * @param args the command line arguments
+     */
+     public static boolean J1= false,J2= false;
+     public static int NJ1=0;
+     public static int NJ2=0;
+     private static int columna=0,fila=0;
+     public String simbolo;
+       //la x dice el turno del jugador
         int x =1;
-        int columna=0,fila=0;
-     public  String variable;
-
-    public int getColumna() {
-        return columna;
-    }
-
-    public void setColumna(int columna) {
-        this.columna = columna;
-    }
-
-    public int getFila() {
-        return fila;
-    }
-
-    public void setFila(int fila) {
-        this.fila = fila;
-    }
-
-   
-
-    public String getVariable() {
-        return variable;
-    }
-
-    public void setVariable(String variable) {
-        this.variable = variable;
-    }
-
-    
-     
-        
     
     public static int MN[][] = new int[3][3];
     public static String MT[][] = new String[3][3];
-    public void matriz(){
-           //inicializamos la matriz en cero
+
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+    
+    
+
+    public static int getColumna() {
+        return columna;
+    }
+
+    public static void setColumna(int columna) {
+        Tres_en_Raya.columna = columna;
+    }
+
+    public static int getFila() {
+        return fila;
+    }
+
+    public static void setFila(int fila) {
+        Tres_en_Raya.fila = fila;
+    }
+    
+    
+    
+    public void main() {
+    
+        //inicializamos la matriz en cero
         for (int i=0;i<3;i++){
             for (int j=0;j<3;j++){
                 MN[i][j]=0;            
             }
+            
         }
+     
     }
-    
-    public void triqui(){
-      int i;
-      int j;
-     for (i=0;i<=2;i++){
-        for (j=0;j<=2;j++){
-            System.out.print("Escribir valor " + i + " , " + j + " : ");
-            MT [i][j] = dato.nextInt();
+    String Vector []=new  String [MT.length];
+    public void mostrar(){
+         if (x==1){
+                MN[fila][columna]=1;
+                x=2;
+                simbolo="X";
+            }else{
+                MN[fila][columna]=-1;
+                simbolo="O";
+                x=1;
+            }
+                      //lleno la matriz que se mostrara al usuario con sus respectivos simbolos
+      
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                switch (MN[i][j]) {
+                    case 0:
+                        MT[i][j]= "||";
+                       
+                        break;
+                    case 1:
+                      MT[i][j]= "|X|";
+                     
+                   
+                       
+                        
+                        break;
+                    default:
+                       MT[i][j]= "|O|";
+                      
+                      
+                       
+                        break;            
+                }
+                
+            }
+          
         }
-    }
+              //monstramos la matriz al usuario
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+               System.out.print(MT[i][j]);
+               
+              
+            }
+            //salto de linea
+            System.out.print("\n");
+          
         
     }
+       
+   
+        
+     
+   }
     
- 
     
     
-    
-    public  void VerificarG(){
+    public static void VerificarG(){
         //verifica si ya existe un ganador
         
         //caso1
         if(MN[0][0]==1 && MN[1][0]==1 && MN[2][0]==1){
+            
             J1=true;
+            JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador1");
         }else if (MN[0][0]==-1 && MN[1][0]==-1 &&MN[2][0]==-1){
             J2=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador2");
         }
         
         
         //caso2   
        if(MN[0][1]==1 && MN[1][1]==1 && MN[2][1]==1){
             J1=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador1");
+             
         }else if (MN[0][1]==-1 && MN[1][1]==-1 &&MN[2][1]==-1){
             J2=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador2");
         }
        
        
        //caso3
         if(MN[0][2]==1 && MN[1][2]==1 && MN[2][2]==1){
             J1=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador1");
         }else if (MN[0][2]==-1 && MN[1][2]==-1 &&MN[2][2]==-1){
             J2=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador2");
         }
         
         
         //caso4
          if(MN[0][0]==1 && MN[0][1]==1 && MN[0][2]==1){
             J1=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador1");
         }else if (MN[0][0]==-1 && MN[0][1]==-1 &&MN[0][2]==-1){
             J2=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador2");
         }
          
          //caso5
           if(MN[1][0]==1 && MN[1][1]==1 && MN[1][2]==1){
             J1=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador1");
         }else if (MN[1][0]==-1 && MN[1][1]==-1 &&MN[1][2]==-1){
             J2=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador2");
         }
           
           //caso6
            if(MN[2][0]==1 && MN[2][1]==1 && MN[2][2]==1){
             J1=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador1");
         }else if (MN[2][0]==-1 && MN[2][1]==-1 &&MN[2][2]==-1){
             J2=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador2");
         }
            
            //caso 7
             if(MN[0][0]==1 && MN[1][1]==1 && MN[2][2]==1){
             J1=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador1");
         }else if (MN[0][0]==-1 && MN[1][1]==-1 &&MN[2][2]==-1){
             J2=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador2");
         }
             //caso 8
              if(MN[0][2]==1 && MN[1][1]==1 && MN[2][0]==1){
             J1=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador1");
         }else if (MN[0][2]==-1 && MN[1][1]==-1 &&MN[2][0]==-1){
             J2=true;
+             JOptionPane.showMessageDialog(null, "Felicidades ganador Jugador2");
         }
         
         
@@ -161,9 +231,29 @@ public class Tres_en_Raya {
             }
         }
           if(!casillas){
+              if (J1=true) {
+                  
+              }
+               if (J2=true) {
+                  
+              }
               J1=true;J2=true;
+               JOptionPane.showMessageDialog(null, "Empatados");
+               
           }
     }
     
+  
+   
+    public String mostarboton(){
+
+          String resultado=simbolo;
+        
+         return resultado;
     }
+ 
+    
+}        
+        
+            
 
